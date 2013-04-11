be20251060608f5cea3e18ef456a31195bfbb6eb
Tiago Godinho <tiagogodinho3@gmail.com>
2013-04-11 01:23:23 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vereado::Application.config.secret_key_base = '9650b5436ef21330157b0f427afb54d14c816a34789aa70f390d823e0d8b02953e6545174de785cb0e46ed1f9b7382f6aab4ab806fd654a0a78822491ee3fce4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vereado::Application.config.session_store :encrypted_cookie_store, key: '_vereado_session'
